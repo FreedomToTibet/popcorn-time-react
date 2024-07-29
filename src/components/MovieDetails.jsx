523627204e435e8dcb7e148530f9cd59d55eebc2
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-07-29 16:49:30 -0700

    some fixes

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -44,7 +44,7 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
@@ -91,7 +91,6 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
           <button className="btn-back" onClick={onCloseMovie}>
             &larr;
           </button>
-          {/* <img src={poster} alt={`Poster of ${movie} movie`} /> */}
           <img src={poster == "N/A" ? "./no-image-available.jpg" : poster} alt={`Poster of ${movie} movie`} />
           <div className="details-overview">
             <h2>{title}</h2>
