7edff7a7d3b7120b424e89ba681bee8747cfe03b
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-08-01 23:24:49 -0700

    Update useMovies.js

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,37 +1,35 @@
-import { useState, useEffect, useRef } from "react";
+import {useState, useEffect, useRef} from 'react';
 
-const KEY = "ed8c84e0";
+const KEY = 'ed8c84e0';
 
 export const useMovies = (query) => {
-	const [movies, setMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
-	const debounceTimeout = useRef(null);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const debounceTimeout = useRef(null);
 
-	useEffect(
+  useEffect(
     function () {
       const controller = new AbortController();
 
       async function fetchMovies() {
         try {
           setIsLoading(true);
-          setError("");
+          setError('');
 
-          const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
+          const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+            signal: controller.signal,
+          });
 
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
+          if (!res.ok) throw new Error('Something went wrong with fetching movies');
 
           const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
+          if (data.Response === 'False') throw new Error('Movie not found');
 
           setMovies(data.Search);
-          setError("");
+          setError('');
         } catch (err) {
-          if (err.name !== "AbortError") {
+          if (err.name !== 'AbortError') {
             console.log(err.message);
             setError(err.message);
           }
@@ -42,26 +40,24 @@ export const useMovies = (query) => {
 
       if (query.length < 3) {
         setMovies([]);
-        setError("");
+        setError('');
         return;
       }
 
-      // fetchMovies();
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
 
-			if (debounceTimeout.current) {
-				clearTimeout(debounceTimeout.current);
-			}
-	
-			debounceTimeout.current = setTimeout(() => {
-				fetchMovies();
-			}, 500);
+      debounceTimeout.current = setTimeout(() => {
+        fetchMovies();
+      }, 500);
 
       return function () {
         controller.abort();
       };
     },
-    [query]
+    [query],
   );
 
-  return { movies, isLoading, error };
+  return {movies, isLoading, error};
 };
